17eb93d3eae71a035f2128c23da9526428a1307c
Vikas Kumar <vikas@MacBook-Pro.local>
2020-02-26 07:16:09 +0530

    second commit

---
+++ src/main/java/com/vikas/springboot/topic/TopicController.java
@@ -0,0 +1,21 @@
+package com.vikas.springboot.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController  {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework","Spring Framework Description"),
+                new Topic("java", "Core Java","Core Java Description"),
+                new Topic("javascript", "JavaScript","JavaScript Description") 
+        ) ;
+    }
+
+}
