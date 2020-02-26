f9dda9a9e6c89c9539678a45460fc21e89910f8c
vikasjhps <vikas.jhps@gmail.com>
2020-02-26 22:59:10 +0530

    Added

--- src/main/java/com/vikas/springboot/topic/TopicController.java
+++ src/main/java/com/vikas/springboot/topic/TopicController.java
@@ -1,21 +1,35 @@
 package com.vikas.springboot.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController  {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework","Spring Framework Description"),
-                new Topic("java", "Core Java","Core Java Description"),
-                new Topic("javascript", "JavaScript","JavaScript Description") 
-        ) ;
+        return topicService.getAllTopics();
     }
 
+    @RequestMapping("/topics/{id}")
+    public Topic  getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(topic,id);
+
+    }
 }
---
+++ src/main/java/com/vikas/springboot/topic/TopicService.java
@@ -0,0 +1,39 @@
+package com.vikas.springboot.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    List<Topic> topics =  new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework","Spring Framework Description"),
+            new Topic("java", "Core Java","Core Java Description"),
+            new Topic("javascript", "JavaScript","JavaScript Description")
+    ) );
+
+    public List<Topic> getAllTopics(){
+        return  topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        for(int i = 0; i < topics.size(); i++){
+            Topic t = topics.get(i);
+            if( t.getId().equals(id)) {
+                topics.set(i,topic);
+                return;
+            }
+        }
+    }
+}
